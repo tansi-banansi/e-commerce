ff99c971e1f79b6d06b8184c5bbfbf8c8ef903e1
Tanya <tanyatodorovaaa@gmail.com>
2024-08-30 18:34:32 +0100

    fix missing heading

--- src/components/YouMightAlsoLike.js
+++ src/components/YouMightAlsoLike.js
@@ -7,8 +7,8 @@ function YouMightAlsoLike() {
         <div>
             <h2 className='font-playfair text-xl md:text-2xl tracking-wide text-charcoal-gray font-bold'>You might also like</h2>
         
-        <div className="flex flex-row w-2/3 mx-auto gap-4 h ">
-            <h2 className='font-playfair text-xl md:text-2xl tracking-wide text-charcoal-gray font-bold'></h2>
+            <div className="flex flex-row w-2/3 mx-auto gap-4 h ">
+            
                 {recommendedProducts.map(product => (
                     <ProductItem key={product.id} product={product} className=""/>
                 ))}
